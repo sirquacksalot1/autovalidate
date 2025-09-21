2a7456ef78ac2b6c8cf308e21d67fd74998e3c36
Kevin Buffardi <kbuffardi@csuchico.edu>
2025-09-21 15:07:03 -0700

    Add initial validation program

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+
+const vector <string> VALIDATION = {"Cool","Great","Perfect","Beautiful"};
+
+int main(){
+  string input;
+  int pick;
+
+  srand(time(0));
+  pick = rand() % 4;
+  cout << "What are you listening to?\n";
+  getline(cin,input);
+  cout << VALIDATION[pick] << "! Let's listen to more\n";
+
+  do{
+    cout << "What's next?\n";
+    getline(cin,input);
+    pick = rand() % 4;
+    cout << VALIDATION[pick] << "!\n";
+  }while( input != "nothing" );
+
+  return 0;
+}
