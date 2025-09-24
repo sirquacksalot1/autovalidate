603862d28b8a95712ad6bbc91d9fdd71fdac560a
Kevin Buffardi <kbuffardi@csuchico.edu>
2025-09-23 21:29:31 -0700

    Convert input to lower case for case insensitivity

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 #include <cstdlib>
 #include <ctime>
 #include <vector>
+#include <cctype>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 using std::vector;
+using std::transform;
 
 const vector <string> VALIDATION = {"Cool","Great","Perfect","Beautiful","Aw, yeah"};
 
@@ -19,11 +21,13 @@ int main(){
   pick = rand() % VALIDATION.size();
   cout << "What are you listening to?\n";
   getline(cin,input);
+  transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
   cout << VALIDATION[pick] << "! Let's listen to more\n";
 
   do{
     cout << "What's next?\n";
     getline(cin,input);
+    transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
     pick = rand() % VALIDATION.size();
     cout << VALIDATION[pick] << "!\n";
   }while( input != "nothing" );
